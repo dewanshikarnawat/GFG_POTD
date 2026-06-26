768e9abc3df2908bc723b1b92f6fb79e56712dd7
DewanshiKarnawat <dewanshikarnawat@gmail.com>
2026-06-26 20:24:26 +0530

    Create Count Matching Subsequences.cpp

---
+++ Medium_Problems/Count Matching Subsequences.cpp
@@ -0,0 +1,27 @@
+//Problem: Count Matching Subsequences
+//Topic: Dynamic Programming (2D DP / Space Optimized DP), String DP, Subsequences.
+//Pattern: Medium
+
+class Solution {
+  public:
+    int countWays(string &s1, string &s2) {
+        const int MOD = 1e9 + 7;
+
+        int n = s1.size();
+        int m = s2.size();
+
+        vector<long long> dp(m + 1, 0);
+        dp[0] = 1;
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = m; j >= 1; j--) {
+                if (s1[i - 1] == s2[j - 1]) {
+                    dp[j] = (dp[j] + dp[j - 1]) % MOD;
+                }
+            }
+        }
+
+        return dp[m];
+        
+    }
+};
