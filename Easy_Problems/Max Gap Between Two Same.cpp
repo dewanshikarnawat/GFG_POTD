325ba753ef484cc209826bda017b9c45d4069f4d
DewanshiKarnawat <dewanshikarnawat@gmail.com>
2026-07-05 22:16:15 +0530

    Create Max Gap Between Two Same.cpp

---
+++ Easy_Problems/Max Gap Between Two Same.cpp
@@ -0,0 +1,18 @@
+//Problem: Max Gap Between Two Same
+//Topic: Hashing / Arrays (First Occurrence)
+//Pattern: Easy
+
+vector<int> first(26, -1);
+        int ans = -1;
+
+        for (int i = 0; i < s.size(); i++) {
+            int idx = s[i] - 'a';
+
+            if (first[idx] == -1) {
+                first[idx] = i;
+            } else {
+                ans = max(ans, i - first[idx] - 1);
+            }
+        }
+
+        return ans;
