0091be3b4491dcab7ed42abb3e11ea9303a0a7bb
DewanshiKarnawat <dewanshikarnawat@gmail.com>
2026-07-14 23:43:42 +0530

    Create Smallest Non-Zero Number.cpp

---
+++ Medium_Problems/Smallest Non-Zero Number.cpp
@@ -0,0 +1,17 @@
+//Problem: Smallest Non-Zero Number
+//Topic: Greedy + Reverse Traversal (Backward Simulation)
+//Pattern: Medium
+
+class Solution {
+  public:
+    int find(vector<int>& arr) {
+        long long need = 0;
+
+        for (int i = arr.size() - 1; i >= 0; i--) {
+            need = (need + arr[i] + 1) / 2;   // ceil((need + arr[i]) / 2)
+        }
+
+        return max(1LL, need);
+        
+    }
+};
