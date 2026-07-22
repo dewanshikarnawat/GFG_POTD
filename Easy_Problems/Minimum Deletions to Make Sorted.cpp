3aaffe2cbbbeca68fa69027482d62b57ab726488
DewanshiKarnawat <dewanshikarnawat@gmail.com>
2026-07-22 23:30:46 +0530

    Create Minimum Deletions to Make Sorted.cpp

---
+++ Easy_Problems/Minimum Deletions to Make Sorted.cpp
@@ -0,0 +1,22 @@
+//Problem: Minimum Deletions to Make Sorted
+//Topic: Dynamic Programming (DP)
+//Pattern: Easy
+
+class Solution {
+  public:
+    int minDeletions(vector<int>& arr) {
+        vector<int> lis;
+        
+        for (int x : arr) {
+            auto it = lower_bound(lis.begin(), lis.end(), x);
+            
+            if (it == lis.end())
+                lis.push_back(x);
+            else
+                *it = x;
+        }
+        
+        return arr.size() - lis.size();
+        
+    }
+};
