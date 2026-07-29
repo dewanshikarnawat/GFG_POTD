2b06842e329ad682a9a132eac2b1bfd0b7e589a8
DewanshiKarnawat <dewanshikarnawat@gmail.com>
2026-07-29 22:51:36 +0530

    Create Split Array into Minimum Subsets.cpp

---
+++ Easy_Problems/Split Array into Minimum Subsets.cpp
@@ -0,0 +1,20 @@
+//Problem: Split Array into Minimum Subsets
+//Topic: Sorting
+//Pattern: Easy
+
+class Solution {
+  public:
+    int minSubsets(vector<int>& arr) {
+        sort(arr.begin(), arr.end());
+
+        int ans = 1;
+
+        for (int i = 1; i < arr.size(); i++) {
+            if (arr[i] != arr[i - 1] + 1)
+                ans++;
+        }
+
+        return ans;
+        
+    }
+};
