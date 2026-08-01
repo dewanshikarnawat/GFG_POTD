6c8ea554391e68d5b21dec78d0d37a18e760ec54
DewanshiKarnawat <dewanshikarnawat@gmail.com>
2026-08-01 21:57:22 +0530

    Create Max After m Range Increments.cpp

---
+++ Medium_Problems/Max After m Range Increments.cpp
@@ -0,0 +1,29 @@
+//Problem: Max After m Range Increments
+//Topic: Difference Array + Prefix Sum + Range Update Queries
+//Pattern: Medium
+
+class Solution {
+  public:
+    int findMax(int n, vector<int>& a, vector<int>& b, vector<int>& k) {
+        vector<long long> diff(n + 1, 0);
+
+        int m = a.size();
+
+        for (int i = 0; i < m; i++) {
+            diff[a[i]] += k[i];
+
+            if (b[i] + 1 < n)
+                diff[b[i] + 1] -= k[i];
+        }
+
+        long long curr = 0, ans = 0;
+
+        for (int i = 0; i < n; i++) {
+            curr += diff[i];
+            ans = max(ans, curr);
+        }
+
+        return (int)ans;
+        
+    }
+};
